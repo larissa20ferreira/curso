90edbc771ba90dcd49de9dab336099851f962d86
larissa20ferreira <larissa.harthkoph.ferreira@escola.pr.gov.br>
2024-07-17 22:25:15 -0300

    Add files via upload

---
+++ larissa/materia_nova.py
@@ -0,0 +1,89 @@
+#import pyautogui
+#pandas
+#streamlit
+#time
+
+import time
+import pyautogui as py
+"""
+#Quando preciso pressionar uma tecla do computador
+py.press('win')
+#Espera 1 segundo
+time.sleep(1)
+#Escreva chrome
+py.write('chrome')
+#Espera 1 segundo
+time.sleep(1)
+#Quando preciso pressionar uma tecla do computador
+py.press('enter')
+#quando precisar pressionar um conjunto de teclas
+#py.hotkey('ctrl,shift,n')
+time.sleep(1)
+#Escreva o site que deseja clicar
+py.write('gmail.com')
+#Quando preciso pressionar uma tecla do computador
+py.press('enter')
+time.sleep(2)
+py.click(x=150, y=175)
+#time.sleep(2)
+#x=214, y=340(x=107 y=265)
+time.sleep(2)
+py.write('larissa.harthkoph.ferreira@escola.pr.gov.br')
+py.press('tab')
+time.sleep(1)
+py.press('tab')
+time.sleep(1)
+py.write('teste')
+time.sleep(1)
+py.click(x=1304, y=1001)"""
+
+
+"""py.press('win')
+py.sleep(1)
+py.write('word')
+py.sleep(1)
+py.press('enter')
+py.sleep(3)
+py.click(x=733, y=359)
+py.sleep(2)
+py.write('Testando atividade!!')
+py.sleep(2)
+#py.hotkey('Ctrl' ,'Shift', 'S')
+py.sleep(1)
+#arquivo
+py.click(x=30, y=46) 
+py.sleep(1)
+#salvar como
+py.click(x=47, y=243)
+py.sleep(1)
+#Procurar
+py.click(x=293, y=370)
+py.sleep(1)
+#Arquivo
+py.click(x=114, y=200)
+py.sleep(1)
+#Nova pasta
+py.click(x=176, y=83)
+py.sleep(1)
+py.write('LARISSA_PYTHON')
+#Abrir
+py.click(x=826, y=532)
+py.sleep(1)
+#Salvar
+py.click(x=826, y=532)
+py.sleep(1)
+py.click(x=826, y=532)"""
+
+
+
+#ExemploProfessor
+#Não terminado
+py.press('win')
+py.sleep(1)
+py.write('bloco de notas')
+py.sleep(1)
+py.press('enter')
+py.sleep(2)
+py.write('Curso de PYTHON do senai')
+py.sleep(1)
+py.hotkey('ctrl', 'shfit', 's')
